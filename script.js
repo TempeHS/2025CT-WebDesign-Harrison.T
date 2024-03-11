a2063c710d256cb66494650d320332f7c0944c93
harry-vt <159206613+harry-vt@users.noreply.github.com>
2024-03-11 14:13:45 +1100

    java script game

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 
-let myIntro = 15;
-let enterName = 13;
+let enterName = prompt("Enter your name"); 
+let playerGuess;
+let playerFeedback = "test";
+let computerGuess;
+loadDiv ()
 
-if (myIntro == enterName) {
-    alert(myIntro);
+function randomNum() {
+    return Math.floor(<)
 }
 
 
+function loadDiv () {
+    document.getElementById("ScriptThisDIV").innerHTML = "<h1> Hello " + enterName + "</h1>"
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<H2> Guess a Number bewteen 1 & 100 </H2>"
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + playerFeedback ; 
+}
 
 
-
+function enterNumber () {
+    playerGuess = prompt ("Enter a Number") 
+}
